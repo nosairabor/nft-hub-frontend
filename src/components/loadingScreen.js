f3cb2160095feffe3ee042da4fe1634492a1c609
Irabor Nosa <nosairaborr@gmail.com>
2024-11-19 22:31:23 +0100

    adjusted the height of the loading screen

--- src/components/loadingScreen.js
+++ src/components/loadingScreen.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function LoadingScreen( ) {
     return (
         <div className="flex flex-col items-center justify-center bg-hero-mobile  md:bg-hero bg-cover bg-no-repeat  h-screen w-screen">
-            <div className="-mt-[80px] md:mt-0 zoom-in-out-element">
+            <div className="-mt-[110px] md:mt-0 zoom-in-out-element">
                 <Image 
                     src={`/images/footer-logo.svg`}
                     width={0}
